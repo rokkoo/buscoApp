687b1e839757eb31079be68b172a3d3747cc6d29
Alfonso <alfonsolasaguirre@gmail.com>
2019-06-11 13:19:49 +0200

    Creamos estado global con las lista de posts y las cargamos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StoreProvider } from 'easy-peasy';
 import { Layout } from 'antd';
 
 // import { Route, Switch, Link } from 'react-router-dom';
@@ -7,22 +8,27 @@ import { Router, Link } from '@reach/router';
 import 'antd/dist/antd.css';
 import './App.css';
 
+import store from './store';
+
 import Header from './components/Header/Header';
 import Content from './components/Content/';
 import Footer from './components/Footer';
 
 function App() {
+  store.dispatch.postsList.loadList();
   return (
-    <Layout className="layout" style={{}}>
-      {/* <Switch>
+    <StoreProvider store={store}>
+      <Layout className="layout" style={{}}>
+        {/* <Switch>
         <Route exact path="/" component={MainHeader} />
         <Route path="/trabajo" component={CentralContent} />
         <Route path="/contact" component={Footer} />
       </Switch> */}
-      <Header />
-      <Content />
-      <Footer />
-    </Layout>
+        <Header />
+        <Content />
+        <Footer />
+      </Layout>
+    </StoreProvider>
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,4 @@
-import React, { useEffect } from 'react';
-import { useActions, useStore } from 'easy-peasy';
-import Job from './Job';
-import { Layout, Row } from 'antd';
-
-const { Content } = Layout;
+import { createStore, thunk, action } from 'easy-peasy';
 
 const Posts = [
   {
@@ -47,23 +42,24 @@ const Posts = [
   }
 ];
 
-const CentralContent = ({ match }) => {
-  const state = useStore(state => state.postsList.list);
-
-  useEffect(() => {
-    // console.log(state);
-  }, [state]);
-
-  return (
-    <Content style={{ padding: '0 50px', marginTop: '15px' }}>
-      Posts {state.length}
-      <div style={{ background: '#fff', padding: 24 }}>
-        <Row gutter={{ md: 0 }}>
-          {state.map(data => data.map((job, i) => <Job key={i} data={job} />))}
-        </Row>
-      </div>
-    </Content>
-  );
+const serverFetch = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(Posts);
+    }, 3000);
+  });
 };
+const stores = createStore({
+  postsList: {
+    list: [],
+    loadList: thunk(async (actions, payload) => {
+      const data = await serverFetch();
+      actions.listLoaded(data);
+    }),
+    listLoaded: action((state, payload) => {
+      state.list.push(payload);
+    })
+  }
+});
 
-export default CentralContent;
+export default stores;
