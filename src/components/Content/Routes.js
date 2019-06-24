8a65bb8292ccb7cf7e08e597524c9977f33ef30a
Alfonso <alfonsolasaguirre@gmail.com>
2019-06-24 18:00:41 +0200

    Movida rutas a nuevo archivo

---
+++ src/components/Content/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Router } from "@reach/router";
+
+import Jobs from "./Jobs";
+import JobDetail from "./JobDetail";
+
+const Routes = () => {
+  return (
+    <Router primary={false}>
+      <Jobs path="/" />
+      <JobDetail path="/trabajo/:jobId" />
+      <JobDetail path="/ofertas" />
+    </Router>
+  );
+};
+
+export default Routes;
